ed45d06e1c2515c46f57a7acc7536ea91b1498e9
이용수 <107594743+lysyeah@users.noreply.github.com>
2023-04-28 12:50:22 +0900

    constraints practice

---
+++ constraints_practice/contraints_practice/contraints_practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  contraints_practice
+//
+//  Created by 이용수 on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ constraints_practice/contraints_practice/contraints_practice/ViewController2.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController2.swift
+//  contraints_practice
+//
+//  Created by 이용수 on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController2 : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
